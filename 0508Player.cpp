c526165043db6fa4aa9b9767949c17dac92bbfcc
vivian0103 <79900014+vivian0103@users.noreply.github.com>
2021-05-08 18:24:50 +0800

    Create 0508Player.cpp

---
+++ 0508Player.cpp
@@ -0,0 +1,27 @@
+#include"Player.h"
+#include<iostream>
+using namespace std;
+Player::Player() {
+
+}
+Player::Player(string nN,char nSy) {
+	Name = nN;
+	Symbol = nSy;
+}
+bool Player::setSymbol(char B[][8],char nSy,int Block) {
+	bool re = false;
+	int x, y;
+	x = ((Block - 1) / 3)*2+1;
+	y = ((Block - 1) % 3) * 2 + 1;
+
+	if(B[x][y] != '0' )
+	{
+		cout << "此格已被填寫過了! 請換格!!!!";
+	}
+	else
+	{
+		B[x][y] = nSy;//填入玩家的代號
+		re = true;
+	}
+	return re;
+}
