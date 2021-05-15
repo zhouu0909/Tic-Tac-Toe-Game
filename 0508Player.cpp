45cc8d7bfdc882f917f42763e2b46e49936014db
vivian0103 <79900014+vivian0103@users.noreply.github.com>
2021-05-15 22:40:34 +0800

    Update 0508Player.cpp

--- 0508Player.cpp
+++ 0508Player.cpp
@@ -10,18 +10,101 @@ Player::Player(string nN,char nSy) {
 }
 bool Player::setSymbol(char B[][8],char nSy,int Block) {
 	bool re = false;
-	int x, y;
-	x = ((Block - 1) / 3) * 2 + 1;
-	y = ((Block - 1) % 3) * 2 + 1;
+	switch (Block) { //找格子
+	case 1:
+		if (B[1][1] != '1') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[1][1] = nSy;//填入玩家的代號
+			re = true;
+		}
+		break;
+	case 2:
+		if (B[1][3] != '2') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[1][3] = nSy;
+			re = true;
+		}
+		break;
+	case 3:
+		if (B[1][5] != '3') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[1][5] = nSy;
+			re = true;
+		}
+		break;
+	case 4:
+		if (B[3][1] != '4') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[3][1] = nSy;
+			re = true;
+		}
+		break;
+	case 5:
+		if (B[3][3] != '5') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[3][3] = nSy;
+			re = true;
+		}
+		break;
+	case 6:
+		if (B[3][5] != '6') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[3][5] = nSy;
+			re = true;
+		}
+		break;
+	case 7:
+		if (B[5][1] != '7') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[5][1] = nSy;
+			re = true;
+		}
+		break;
+	case 8:
+		if (B[5][3] != '8') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[5][3] = nSy;
+			re = true;
+		}
+		break;
+	case 9:
+		if (B[5][5] != '9') {
+			cout << "此格已被填寫過了! 請換格!!!!";
+		}
+		else
+		{
+			B[5][5] = nSy;
+			re = true;
+		}
+		break;
 
-	if(B[x][y] != '0' )
-	{
-		cout << "此格已被填寫過了! 請換格!!!!";
-	}
-	else
-	{
-		B[x][y] = nSy;//填入玩家的代號
-		re = true;
+	default:
+		break;
+
 	}
 	return re;
 }
